1b8936d819404d40d9be4fbcd186d79995623641
Wil Kong <alpha0422@gmail.com>
2019-03-17 19:46:21 -0700

    Add tanh function test.

--- tests/test_activation.py
+++ tests/test_activation.py
@@ -18,6 +18,14 @@ class ActivationFunctionTest(unittest.TestCase):
     
         self.assertTrue(torch.autograd.gradcheck(sigmoid, (x,)))
 
+    def test_TanhFunction(self):
+        batch_size, hidden_size = 16, 27
+        tanh = TanhFunction.apply
+        x = torch.randn([batch_size, hidden_size], device='cuda:0', dtype=torch.float64, requires_grad=True)
+   
+        self.assertTrue(torch.autograd.gradcheck(tanh, (x,)))
+
+
 if __name__ == '__main__':
     script_path = os.path.dirname(os.path.realpath(__file__))
     unittest.main()
