eead7688194266bcf0a736dd49c601a01e2aa986
Wil Kong <alpha0422@gmail.com>
2019-03-17 19:37:07 -0700

    Add sigmoid function test.

---
+++ tests/test_activation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+import random
+import torch
+import unittest
+
+from bprop import *
+
+class ActivationFunctionTest(unittest.TestCase):
+    def setUp(self):
+        torch.cuda.manual_seed(1234)
+
+    def test_SigmoidFunction(self):
+        batch_size, hidden_size = 16, 27
+        sigmoid = SigmoidFunction.apply
+        x = torch.randn([batch_size, hidden_size], device='cuda:0', dtype=torch.float64, requires_grad=True)
+    
+        self.assertTrue(torch.autograd.gradcheck(sigmoid, (x,)))
+
+if __name__ == '__main__':
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    unittest.main()
+
