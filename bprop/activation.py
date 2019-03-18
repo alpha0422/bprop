8ca7d9d303621428902682ec1e63f956e0a84fce
Wil Kong <alpha0422@gmail.com>
2019-03-17 19:45:49 -0700

    Add tanh function bprop.

--- bprop/activation.py
+++ bprop/activation.py
@@ -15,3 +15,16 @@ class SigmoidFunction(torch.autograd.Function):
         grad_x = torch.mul(torch.mul(y, 1-y), grad_y)
         return grad_x
 
+class TanhFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x):
+        y = torch.div(torch.exp(x)-torch.exp(-x), torch.exp(x)+torch.exp(-x))
+        ctx.save_for_backward(y)
+        return y
+
+    @staticmethod
+    def backward(ctx, grad_y):
+        y, = ctx.saved_tensors
+        grad_x = torch.mul(1 - torch.pow(y, 2), grad_y)
+        return grad_x
+
