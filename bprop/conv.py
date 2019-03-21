6d892c3d8d6f4aeb2558e9c699a89c5e33f93715
Wil Kong <alpha0422@gmail.com>
2019-03-20 19:44:39 -0700

    Add group convolution bprop.

--- bprop/conv.py
+++ bprop/conv.py
@@ -5,7 +5,7 @@ import itertools
 
 class Convolution2DFunctionImpl1(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, inputs, filters, bias, strides=(1,1), padding=(0,0), dilation=(1,1)):
+    def forward(ctx, inputs, filters, bias, strides=(1,1), padding=(0,0), dilation=(1,1), groups=1):
         def check(tup, lb):
             if len(tup) == 2 and isinstance(tup[0], int) and isinstance(tup[1], int) \
                 and tup[0] >= lb and tup[1] >= lb:
@@ -13,19 +13,21 @@ class Convolution2DFunctionImpl1(torch.autograd.Function):
             else:
                 return False
 
-        assert inputs.size(1) == filters.size(1), "Input channel mismatch!"
+        assert inputs.size(1) // groups == filters.size(1), "Input channel mismatch!"
         assert filters.size(0) == bias.size(0), "Output channel mismatch!"
         assert check(strides, 1), "Invalid strides!"
         assert check(padding, 0), "Invalid padding!"
         assert check(dilation, 1), "Invalid dilation!"
+        assert filters.size(0) % groups == 0, "Invalid groups!"
         
         N, C, H, W = inputs.shape
-        K, C, R, S = filters.shape
+        K, R, S = filters.shape[0], filters.shape[2], filters.shape[3]
         U, V = strides
         PH, PW = padding
         DH, DW = dilation
         DR = DH*(R-1) + 1
         DS = DW*(S-1) + 1
+        G = groups
 
         P = (H + 2*PH - DH*(R-1) - 1) // U + 1
         Q = (W + 2*PW - DW*(S-1) - 1) // V + 1
@@ -36,23 +38,25 @@ class Convolution2DFunctionImpl1(torch.autograd.Function):
         pinputs[:,:,PH:H+PH,PW:W+PW] = inputs  # PH:-PH is wrong if PH is 0
 
         for n,k,p,q in itertools.product(range(N),range(K),range(P),range(Q)):
-            outputs[n,k,p,q] = torch.sum(pinputs[n,:,p*U:p*U+DR:DH,q*V:q*V+DS:DW] * filters[k,...]) + bias[k]
+            outputs[n,k,p,q] = torch.sum(pinputs[n,k//G*C//G:(k//G+1)*C//G,p*U:p*U+DR:DH,q*V:q*V+DS:DW] * filters[k,...]) + bias[k]
 
         ctx.save_for_backward(inputs, filters, bias, torch.IntTensor(strides),
-            torch.IntTensor(padding), torch.IntTensor(dilation))
+            torch.IntTensor(padding), torch.IntTensor(dilation),
+            torch.IntTensor([groups]))
         return outputs
 
     @staticmethod
     def backward(ctx, gO):
-        inputs, filters, bias, strides, padding, dilation = ctx.saved_tensors
+        inputs, filters, bias, strides, padding, dilation, groups = ctx.saved_tensors
 
         N, C, H, W = inputs.shape
-        K, C, R, S = filters.shape
+        K, R, S = filters.shape[0], filters.shape[2], filters.shape[3]
         U, V = strides
         PH, PW = padding
         DH, DW = dilation
         DR = DH*(R-1) + 1
         DS = DW*(S-1) + 1
+        G = groups
 
         P = (H + 2*PH - DH*(R-1) - 1) // U + 1
         Q = (W + 2*PW - DW*(S-1) - 1) // V + 1
@@ -65,9 +69,9 @@ class Convolution2DFunctionImpl1(torch.autograd.Function):
         pinputs[:,:,PH:H+PH,PW:W+PW] = inputs
 
         for n,k,p,q in itertools.product(range(N),range(K),range(P),range(Q)):
-            gI[n,:,p*U:p*U+DR:DH,q*V:q*V+DS:DW] += filters[k,...] * gO[n,k,p,q]
-            gW[k,...] += gO[n,k,p,q] * pinputs[n,:,p*U:p*U+DR:DH,q*V:q*V+DS:DW]
+            gI[n,k//G*C//G:(k//G+1)*C//G,p*U:p*U+DR:DH,q*V:q*V+DS:DW] += filters[k,...] * gO[n,k,p,q]
+            gW[k,...] += gO[n,k,p,q] * pinputs[n,k//G*C//G:(k//G+1)*C//G,p*U:p*U+DR:DH,q*V:q*V+DS:DW]
             gb[k] += gO[n,k,p,q]
 
-        return gI[:,:,PH:H+PH,PW:W+PW], gW, gb, None, None, None
+        return gI[:,:,PH:H+PH,PW:W+PW], gW, gb, None, None, None, None
 
--- tests/test_conv.py
+++ tests/test_conv.py
@@ -13,36 +13,38 @@ class Convolution2DFunctionTest(unittest.TestCase):
 
     def test_Convolution2DFunctionImpl1_fprop(self):
         N, C, H, W = 3, 2, 7, 5
-        K, R, S = 2, 3, 2
+        K, R, S = 4, 3, 2
         U, V = 1, 3
         PH, PW = 2, 3
         DH, DW = 2, 1
+        G = 2
 
         conv = Convolution2DFunctionImpl1.apply
 
         x = torch.randn([N,C,H,W], dtype=torch.float64, requires_grad=True)
-        filters = torch.randn([K,C,R,S], dtype=torch.float64, requires_grad=True)
+        filters = torch.randn([K,C//G,R,S], dtype=torch.float64, requires_grad=True)
         bias = torch.randn([K], dtype=torch.float64, requires_grad=True)
    
-        ref_y = torch.nn.functional.conv2d(x, filters, bias, (U,V), (PH,PW), (DH,DW))
-        tst_y = conv(x, filters, bias, (U,V), (PH,PW), (DH,DW))
+        ref_y = torch.nn.functional.conv2d(x, filters, bias, (U,V), (PH,PW), (DH,DW), G)
+        tst_y = conv(x, filters, bias, (U,V), (PH,PW), (DH,DW), G)
 
         self.assertTrue(torch.allclose(ref_y, tst_y))
 
     def test_Convolution2DFunctionImpl1_bprop(self):
         N, C, H, W = 4, 3, 5, 6
-        K, R, S = 3, 2, 3
+        K, R, S = 6, 2, 3
         U, V = 2, 1
         PH, PW = 2, 0
         DH, DW = 1, 2
+        G = 3
 
         conv = Convolution2DFunctionImpl1.apply
 
         x = torch.randn([N,C,H,W], dtype=torch.float64, requires_grad=True)
-        filters = torch.randn([K,C,R,S], dtype=torch.float64, requires_grad=True)
+        filters = torch.randn([K,C//G,R,S], dtype=torch.float64, requires_grad=True)
         bias = torch.randn([K], dtype=torch.float64, requires_grad=True)
    
-        self.assertTrue(torch.autograd.gradcheck(conv, (x, filters, bias, (U,V), (PH,PW), (DH,DW))))
+        self.assertTrue(torch.autograd.gradcheck(conv, (x, filters, bias, (U,V), (PH,PW), (DH,DW), G)))
 
 if __name__ == '__main__':
     script_path = os.path.dirname(os.path.realpath(__file__))
